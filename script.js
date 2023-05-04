41b9e60583673ff6a5b57f1637d84288a102864b
Zohaib <zrana1214@gmail.com>
2023-05-03 23:39:17 -0400

    added 404 handling

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const search = document.getElementById('search')
 const weather_info = document.querySelector('.info-box')
+const error = document.querySelector('.error')
 
-
-
-var visible = false
+// const sun = document.getElementById('sun')
 
 search.addEventListener('click', () => {
 
@@ -11,14 +10,11 @@ search.addEventListener('click', () => {
     const APIkey = '09beed1c29719955be81f477691140e2'
     const city = document.querySelector('.search-input').value
 
+    // sun.style.display = 'flex'
 
 
     if(!city){
         //TODO code here will return a 'not found' error
-
-        // console.log('null')
-
-
 
         weather_info.style.height = '0px'
     } else if(city){
@@ -29,62 +25,66 @@ search.addEventListener('click', () => {
             .then(Response => Response.json())
             .then(json => {
 
-                // `${parseInt(json.main.temp)}<span>°C</span>`
 
-
+                // Error 404
+                console.log(json.cod)
+                if(json.cod === '404'){
+                    error.style.display = 'block'
+                    error.innerHTML = 'Invalid location'
+                    return
+                }
 
                 const temp_info = document.querySelector('.temp')
                 const temp_desc = document.querySelector('.temp-desc')
                 const humidity = document.querySelector('.humidity span')
                 const wind = document.querySelector('.wind span')
-                const img = document.querySelector('.temp-img')
 
                 console.log(json.weather[0].main)
 
+                // TODO font awesome not showing up [FIXED]
                 switch (json.weather[0].main) {
                     case 'Clear':
-                        img.innerHTML = '<i class="fa-solid fa-sun"></i>'
+                        // img.innerHTML = '<i class="fa-solid fa-sun"></i>'
+                        var img = document.getElementById('sun')
+                        img.style.display = 'flex'
                         break;
 
                     case 'Rain':
-                        img.innerHTML = '<i class="fa-solid fa-cloud-showers-heavy"></i>'
+                        // img.innerHTML = '<i class="fa-solid fa-cloud-showers-heavy"></i>'
+                        var img = document.getElementById('rain')
+                        img.style.display = 'flex'
                         break;
 
                     case 'Snow':
-                        img.innerHTML = '<i class="fa-solid fa-snowflake"></i>'
+                        // img.innerHTML = '<i class="fa-solid fa-snowflake"></i>'
+                        var img = document.getElementById('snow')
+                        img.style.display = 'flex'
                         break;
 
                     case 'Clouds':
-                        img.innerHTML = `<i class="fa-solid fa-clouds"></i>`
-                        console.log('in clear')
-                        console.log(img)
+                        // img.innerHTML = `<i class="fa-solid fa-clouds"></i>`
+                        var img = document.getElementById('cloud')
+                        img.style.display = 'flex'
                         break;
 
                     case 'Haze':
                         img.innerHTML = '<i class="fa-solid fa-sun-haze"></i>'
+                        var img = document.getElementById('haze')
+                        img.style.display = 'flex'
                         break;
 
                     default:
                         img.innerHTML = '<i class="fa-solid fa-sun-haze"></i>';
                 }
 
-
-
-
                 temp_info.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`
                 temp_desc.innerHTML = `${json.weather[0].description}`
                 humidity.innerHTML = `${parseInt(json.main.humidity)}%`
                 wind.innerHTML = `${parseInt(json.wind.speed)}M/h`
+
+
+                weather_info.style.height = '75%'
             })
-
-
-
-        // temp_info.innerHTML = capitalizeFirstLetter(city)
-
-
-
-        weather_info.style.height = '75%'
-
     }
 
 })
