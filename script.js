9b2840fb29960ad2641cdd903a09528f99e8b7ff
Zohaib <zrana1214@gmail.com>
2023-05-03 22:26:22 -0400

    working on visuals

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const search = document.getElementById('search')
 const weather_info = document.querySelector('.info-box')
-const temp_info = document.querySelector('.temp-info')
+
 
 
 var visible = false
@@ -25,15 +25,59 @@ search.addEventListener('click', () => {
         //TODO return the weather info
 
 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`)
             .then(Response => Response.json())
             .then(json => {
-                console.log(`temp is ${json.main.temp}`)
+
+                // `${parseInt(json.main.temp)}<span>°C</span>`
+
+
+
+                const temp_info = document.querySelector('.temp')
+                const temp_desc = document.querySelector('.temp-desc')
+                const humidity = document.querySelector('.humidity span')
+                const wind = document.querySelector('.wind span')
+                const img = document.querySelector('.temp-img')
+
+
+                switch (json.weather[0].main) {
+                    case 'Clear':
+                        // image.src = 'images/clear.png';
+                        // <i class="fa-solid fa-sun"></i>
+                        break;
+
+                    case 'Rain':
+                        // image.src = 'images/rain.png';
+                        break;
+
+                    case 'Snow':
+                        // image.src = 'images/snow.png';
+                        break;
+
+                    case 'Clouds':
+                        image.src = 'images/cloud.png';
+                        break;
+
+                    case 'Haze':
+                        image.src = 'images/mist.png';
+                        break;
+
+                    default:
+                        image.src = '';
+                }
+
+
+
+
+                temp_info.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`
+                temp_desc.innerHTML = `${json.weather[0].description}`
+                humidity.innerHTML = `${parseInt(json.main.humidity)}%`
+                wind.innerHTML = `${parseInt(json.wind.speed)}M/h`
             })
 
 
 
-        temp_info.innerHTML = capitalizeFirstLetter(city)
+        // temp_info.innerHTML = capitalizeFirstLetter(city)
 
 
 
